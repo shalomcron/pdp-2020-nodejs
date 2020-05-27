4f960e16cb5ad5b3242b2c18d0fa1031f5e0c49d
shalom <shalomcron@gmail.com>
2020-05-27 16:26:06 +0300

    add express project

--- routes/articles.js
+++ routes/articles.js
@@ -3,12 +3,18 @@ var router = express.Router();
 var articles = require('../db/articles');
 
 router.get('/', (req, res) => {
-    articles.find().then((result) => {
-        console.log("articles.find result", result);
-        res.send(result);
+    articles.aggragatedFind().then((result) => {
+        res.send({articles: result});
     }, function(err) {
         console.log('articles.find ERR', err);
     });
+
+    // articles.find().then((result) => {
+    //     console.log("articles.find result", result);
+    //     res.send(result);
+    // }, function(err) {
+    //     console.log('articles.find ERR', err);
+    // });
 });
 
 router.get('/agg', (req, res) => {
