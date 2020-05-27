092b11c49e6ee83bb8f198899fb93ffe716484dc
shalom <shalomcron@gmail.com>
2020-05-27 22:23:20 +0300

    add express project

--- routes/articles.js
+++ routes/articles.js
@@ -4,6 +4,8 @@ var articles = require('../db/articles');
 
 router.get('/', (req, res) => {
     articles.aggragatedFind().then((result) => {
+        res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+        res.header("Access-Control-Allow-Methods", "POST,GET,OPTIONS, PUT, DELETE");
         res.send({articles: result});
     }, function(err) {
         console.log('articles.find ERR', err);
