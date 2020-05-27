5b7c09cee1b9abcbaba9dec2b75d813657a06e51
shalom <shalomcron@gmail.com>
2020-05-27 22:25:51 +0300

    add express project

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/*',function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/articles', articlesRouter)
